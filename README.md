272d938d883cf52dae4f9418d80acd75a8d1b195
Bao, Zhenzhe <zhenzhe.bao@uconn.edu>
2026-01-22 17:04:27 -0500

    Updated readme and boxplot.py
    Updated readme with more information and completed boxplot.py

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # A01
-This is my answer for assignment 1.
+Program overview 
+This program allows you to create Box Plots for California Housing dataset.
+
+About dataset:
+From the information online:
+California Housing dataset contains the median house value as target variable and the following input variables (features): average income, housing average age, average rooms, average bedrooms, population, average occupation, latitude, and longitude.
 
 
-I will run the script to get output for California Housing dataset.
+How to run run this script:
+install the requirement txt file first:
+pip install -r requirements.txt
+then run the script with following code:
+python src/boxplot.py
+
+Option:
+When the script runs, it will list the column header for this dataset, 
+you can type all to get Box Plot for all columns except latitude and longitude columns, or use
+the number before column header to select a specific one or type exit to stop the script from running. The generated box plot will be saved in the figs folder.
 
--- src/boxplot.py
+++ src/boxplot.py
@@ -1,5 +1,8 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Load California Housing dataset
 housing = fetch_california_housing(as_frame=True)
@@ -7,8 +10,97 @@ housing = fetch_california_housing(as_frame=True)
 # Features + target as a single DataFrame
 df = housing.frame
 
+##########################################################################################
 # Quick check
+"""
 print(df.head())
 print(df.shape)
 
-# you can save the boxplot...
+# check the column header
+print(df.columns)
+#check null value in the dataset and data type in each column
+print("\n")
+print(df.info())
+print("\nNumber of duplicated records in the dataset:",df.duplicated().sum())
+
+#check the correlation between each column
+df.corr()
+
+# some descriptive statistics for the dataset
+df.describe()
+
+# calculate the variance for each columns in 2 decimal places
+round(df.var(),2)
+
+# show the data distributuon for each column, except latitude and longitude column
+
+for i in df.columns:
+  if i== "Latitude" or i=="Longitude":
+    continue
+  else:
+    sns.histplot(df[i],kde=True)
+    plt.title(f"Histogram for column:{i}")
+    plt.show()"""
+#############################################################################################
+
+#Creating Box plot for each column, except latitude and longitude column
+'''print("\n")
+print("Generating the Box plot for each column, except latitude and longitude column")
+for i in df.columns:
+  if i =="Latitude" or i=="Longitude":
+    continue
+  else:
+    sns.boxplot(df[i])
+    plt.savefig(f"./figs/Box Plot_{i}.png")
+    plt.title(f"Box Plot for column:{i}")
+    plt.show()'''
+
+############################################################################################
+#Assignment Solution
+print("\n")
+print("\n")
+print("These are the columns in dataset, you can type all to get box plot for all columns, except latitude and longitude column.")
+print("You also can use the number before column header to choose a particular column.")
+print("Type exit to quit the program.")
+print("\n")
+print("These are the columns in dataset:")
+number=0
+for i in df.columns:
+  print(number,".",i)
+  number=number+1
+print("\n")
+
+choice=True
+while choice !=False:
+  print("Which column would you like to choose for Box Plot?")
+  userinput =input()
+  if userinput=='exit':
+    print("You quit this program.")
+    break
+  if userinput=='all':
+    print("\n")
+    print("Generating the Box plot for each column, except latitude and longitude column")
+    print("\n")
+    for i in df.columns:
+      if i =="Latitude" or i=="Longitude":
+        continue
+      else:
+        sns.boxplot(df[i])
+        plt.savefig(f"./figs/Box Plot_{i}.png")
+        plt.title(f"Box Plot for column:{i}")
+        plt.show()   
+  else:
+        choice=int(userinput)
+        if choice < 0 or choice > (len(df.columns)-1):
+              print("Please provide a correct number.")
+              print("\n")
+              continue
+        else:
+              sns.boxplot(df[df.columns[choice]])
+              plt.savefig(f"./figs/Box Plot_{df.columns[choice]}.png")
+              plt.title(f"Box Plot for column:{df.columns[choice]}")
+              plt.show()
+              print("\n")
+              continue
+
+
